96c247e57bc849b811dcdc5cfa91f321319df81d
Никита Смирнов <152095229+N1smi@users.noreply.github.com>
2025-09-20 22:01:00 +0300

    Working with the MVector class. Overloading arithmetic operations. Overloading operations to access an array (blank for triangular matrices). Writing a constructor with a new start_index variable. Writing tests.

--- lib_mathvector/mathvector.h
+++ lib_mathvector/mathvector.h
@@ -4,16 +4,36 @@
 #define LIB_MVECTOR_MVECTOR_H_
 
 #include "../lib_tvector/tvector.h"
+#include<stdexcept>
 
 template <class T>
 class MVector : public TVector<T> {
-
+  size_t _start_index = 0;
 public:
+  MVector(size_t size, size_t start_index);
   MVector(size_t size = 0);
   MVector(std::initializer_list<T>);
   MVector(const MVector<T>& other);
   ~MVector();
+
+  inline size_t start_index() const noexcept;
+
+  const T& operator[](size_t index) const;
+  T& operator[](size_t index);
+
+  inline T& at(size_t index);
+
+  MVector<T>& operator+=(const MVector<T>& vec);
+  MVector<T>& operator-=(const MVector<T>& vec);
+  MVector<T>& operator*=(T val);
+  MVector<T> operator+(MVector<T> vec);
+  MVector<T> operator-(MVector<T> vec);
+  MVector<T> operator*(T val);
+  T operator*(MVector<T> vec);
 };
+
+template <class T>
+MVector<T>::MVector(size_t size, size_t start_index) : TVector<T>(size), _start_index(start_index) {}
 
 template <class T>
 MVector<T>::MVector(size_t size) : TVector<T>(size) {}
@@ -22,10 +42,116 @@ template <class T>
 MVector<T>::MVector(std::initializer_list<T> initList) : TVector<T>(initList) {}
 
 template <class T>
-MVector<T>::MVector(const MVector<T>& other) : TVector<T>(other) {}
+MVector<T>::MVector(const MVector<T>& other) : TVector<T>(other), _start_index(other._start_index) {}
 
 template <class T>
 MVector<T>::~MVector() = default;
+
+template <class T>
+inline size_t MVector<T>::start_index() const noexcept {
+  return _start_index;
+}
+
+template <class T>
+const T& MVector<T>::operator[](size_t index) const {
+  if (index < _start_index) {
+    static const T zero = T();
+    return zero;
+  }
+  return TVector<T>::operator[](index - _start_index);
+}
+
+template <class T>
+T& MVector<T>::operator[](size_t index) {
+  if (index < _start_index) {
+    static T zero = T();
+    return zero;
+  }
+  return TVector<T>::operator[](index - _start_index);
+}
+
+template <class T>
+inline T& MVector<T>::at(size_t index) {
+  if (index >= _start_index + this->size()) {
+    throw std::out_of_range("Index out of range in at()");
+  }
+  if (index < _start_index) {
+    static T zero = T();
+    return zero;
+  }
+  
+  return TVector<T>::at(index - _start_index);
+}
+
+template <class T>
+MVector<T>& MVector<T>::operator+=(const MVector<T>& vec) {
+  if (this->size() != vec.size()) {
+    throw std::logic_error("Size error: vectors cannot have different sizes in the addition operation!");
+  }
+  for (size_t i = 0; i < vec.size(); i++) {
+    (*this)[i] += vec[i];
+  }
+
+  return *this;
+}
+
+template <class T>
+MVector<T>& MVector<T>::operator-=(const MVector<T>& vec) {
+  if (this->size() != vec.size()) {
+    throw std::logic_error("Size error: vectors cannot have different sizes in the subtraction operation!");
+  }
+
+  for (size_t i = 0; i < vec.size(); i++) {
+    (*this)[i] -= vec[i];
+  }
+
+  return *this;
+}
+
+template <class T>
+MVector<T>& MVector<T>::operator*=(T val) {
+  for (size_t i = 0; i < (*this).size(); i++) {
+    (*this)[i] *= val;
+  }
+
+  return *this;
+}
+
+template <class T>
+MVector<T> MVector<T>::operator+(MVector<T> vec) {
+  MVector<T> result = *this;
+  result += vec;
+  return result;
+}
+
+template <class T>
+MVector<T> MVector<T>::operator-(MVector<T> vec) {
+  MVector<T> result = *this;
+  result -= vec;
+  return result;
+}
+
+template <class T>
+MVector<T> MVector<T>::operator*(T val) {
+  MVector<T> result = *this;
+  result *= val;
+  return result;
+}
+
+template <class T>
+T MVector<T>::operator*(MVector<T> vec) {
+  if (this->size() != vec.size()) {
+    throw std::logic_error("Size error: vectors cannot have different sizes in the multiplication operation!");
+  }
+
+  T result = T();
+
+  for (size_t i = 0; i < vec.size(); i++) {
+    result += (*this)[i] * vec[i];
+  }
+
+  return result;
+}
 
 
 #endif // LIB_MVECTOR_MVECTOR_H_
--- tests/test_mathvector.cpp
+++ tests/test_mathvector.cpp
@@ -2,3 +2,190 @@
 
 #include <gtest/gtest.h>
 #include "../lib_mathvector/mathvector.h"
+
+TEST(TestMathVectorLib, CreateVectorWithStartIndex) {
+  MVector<int> vec(5, 2);
+
+  EXPECT_EQ(vec.size(), 5);
+  EXPECT_EQ(vec.start_index(), 2);
+  EXPECT_NE(vec.data(), nullptr);
+}
+
+TEST(TestMathVectorLib, OperatorPlusEqual) {
+  MVector<int> vec1 = { 1, 2, 3 };
+  MVector<int> vec2 = { 4, 5, 6 };
+
+  vec1 += vec2;
+
+  EXPECT_EQ(vec1.size(), 3);
+  EXPECT_EQ(vec1[0], 5);
+  EXPECT_EQ(vec1[1], 7);
+  EXPECT_EQ(vec1[2], 9);
+
+  EXPECT_EQ(vec2[0], 4);
+  EXPECT_EQ(vec2[1], 5);
+  EXPECT_EQ(vec2[2], 6);
+}
+
+TEST(TestMathVectorLib, OperatorPlusEqualThrowWhenDifferentSizes) {
+  MVector<int> vec1 = { 1, 2, 3 };
+  MVector<int> vec2 = { 1, 2 };
+
+  EXPECT_ANY_THROW(vec1 += vec2);
+}
+
+TEST(TestMathVectorLib, OperatorPlus) {
+  MVector<int> vec1 = { 1, 2, 3 };
+  MVector<int> vec2 = { 4, 5, 6 };
+
+  MVector<int> result = vec1 + vec2;
+
+  EXPECT_EQ(result.size(), 3);
+  EXPECT_EQ(result[0], 5);
+  EXPECT_EQ(result[1], 7);
+  EXPECT_EQ(result[2], 9);
+
+  EXPECT_EQ(vec1[0], 1);
+  EXPECT_EQ(vec1[1], 2);
+  EXPECT_EQ(vec1[2], 3);
+  EXPECT_EQ(vec2[0], 4);
+  EXPECT_EQ(vec2[1], 5);
+  EXPECT_EQ(vec2[2], 6);
+}
+
+TEST(TestMathVectorLib, OperatorMinusEqual) {
+  MVector<int> vec1 = { 10, 20, 30 };
+  MVector<int> vec2 = { 4, 5, 6 };
+
+  vec1 -= vec2;
+
+  EXPECT_EQ(vec1.size(), 3);
+  EXPECT_EQ(vec1[0], 6);
+  EXPECT_EQ(vec1[1], 15);
+  EXPECT_EQ(vec1[2], 24);
+
+  EXPECT_EQ(vec2[0], 4);
+  EXPECT_EQ(vec2[1], 5);
+  EXPECT_EQ(vec2[2], 6);
+}
+
+TEST(TestMathVectorLib, OperatorMinusEqualThrowWhenDifferentSizes) {
+  MVector<int> vec1 = { 1, 2, 3 };
+  MVector<int> vec2 = { 1, 2 };
+
+  EXPECT_ANY_THROW(vec1 -= vec2);
+}
+
+TEST(TestMathVectorLib, OperatorMinus) {
+  MVector<int> vec1 = { 10, 20, 30 };
+  MVector<int> vec2 = { 4, 5, 6 };
+
+  MVector<int> result = vec1 - vec2;
+
+  EXPECT_EQ(result.size(), 3);
+  EXPECT_EQ(result[0], 6);
+  EXPECT_EQ(result[1], 15);
+  EXPECT_EQ(result[2], 24);
+
+  EXPECT_EQ(vec1[0], 10);
+  EXPECT_EQ(vec1[1], 20);
+  EXPECT_EQ(vec1[2], 30);
+  EXPECT_EQ(vec2[0], 4);
+  EXPECT_EQ(vec2[1], 5);
+  EXPECT_EQ(vec2[2], 6);
+}
+
+TEST(TestMathVectorLib, OperatorMultiplyEqualScalar) {
+  MVector<int> vec = { 1, 2, 3, 4 };
+
+  vec *= 2;
+
+  EXPECT_EQ(vec.size(), 4);
+  EXPECT_EQ(vec[0], 2);
+  EXPECT_EQ(vec[1], 4);
+  EXPECT_EQ(vec[2], 6);
+  EXPECT_EQ(vec[3], 8);
+}
+
+TEST(TestMathVectorLib, OperatorMultiplyScalar) {
+  MVector<int> vec = { 1, 2, 3 };
+
+  MVector<int> result = vec * 3;
+
+  EXPECT_EQ(result.size(), 3);
+  EXPECT_EQ(result[0], 3);
+  EXPECT_EQ(result[1], 6);
+  EXPECT_EQ(result[2], 9);
+
+  EXPECT_EQ(vec[0], 1);
+  EXPECT_EQ(vec[1], 2);
+  EXPECT_EQ(vec[2], 3);
+}
+
+TEST(TestMathVectorLib, OperatorMultiply) {
+  MVector<int> vec1 = { 1, 2, 3 };
+  MVector<int> vec2 = { 4, 5, 6 };
+
+  int result = vec1 * vec2;
+
+  EXPECT_EQ(result, 32);
+}
+
+TEST(TestMathVectorLib, OperatorMultiplyThrowWhenDifferentSizes) {
+  MVector<int> vec1 = { 1, 2, 3 };
+  MVector<int> vec2 = { 1, 2 };
+
+  EXPECT_ANY_THROW(vec1 * vec2);
+}
+
+TEST(TestMathVectorLib, OperatorBracketsWithStartIndex) {
+  MVector<int> vec(3, 2);
+
+  vec[2] = 10;
+  vec[3] = 20;
+  vec[4] = 30;
+
+  EXPECT_EQ(vec[2], 10);
+  EXPECT_EQ(vec[3], 20);
+  EXPECT_EQ(vec[4], 30);
+}
+
+TEST(TestMathVectorLib, OperatorBracketsWithStartIndexZero) {
+  MVector<int> vec(3, 2);
+
+  vec[2] = 10;
+  vec[3] = 20;
+  vec[4] = 30;
+
+  EXPECT_EQ(vec[0], 0);
+  EXPECT_EQ(vec[1], 0);
+}
+
+TEST(TestMathVectorLib, AtWithSrartIndexZero) {
+  MVector<int> vec(3, 2);
+
+  EXPECT_EQ(vec.at(0), 0);
+  EXPECT_EQ(vec.at(1), 0);
+}
+
+TEST(TestMathVectorLib, AtWithSrartIndexThrowWhenUpperIndices) {
+  MVector<int> vec(3, 2);
+
+  EXPECT_THROW(vec.at(5), std::out_of_range);
+  EXPECT_THROW(vec.at(6), std::out_of_range);
+  EXPECT_THROW(vec.at(100), std::out_of_range);
+}
+
+TEST(TestMathVectorLib, AtWithStartIndex) {
+  MVector<int> vec(3, 2);
+
+  vec.at(2) = 10;
+  vec.at(3) = 20;
+  vec.at(4) = 30;
+
+  EXPECT_EQ(vec.at(2), 10);
+  EXPECT_EQ(vec.at(3), 20);
+  EXPECT_EQ(vec.at(4), 30);
+}
+
+
