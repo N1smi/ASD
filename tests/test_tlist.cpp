4b27155c6bd3e585d479dd5049d5ccb6a7db69c3
Никита Смирнов <152095229+N1smi@users.noreply.github.com>
2025-10-25 17:46:09 +0300

    test: add default constructor test.

--- tests/test_tlist.cpp
+++ tests/test_tlist.cpp
@@ -2,3 +2,11 @@
 
 #include <gtest/gtest.h>
 #include "../lib_tlist/tlist.h"
+
+TEST(TestTList, CreateDefaultList) {
+  TList<int> list;
+
+  EXPECT_TRUE(list.is_empty());
+  EXPECT_EQ(list.head(), nullptr);
+  EXPECT_EQ(list.tail(), nullptr);
+}
