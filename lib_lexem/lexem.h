c4ab4d4536da44349e0630313576a296897f7e77
Никита Смирнов <152095229+N1smi@users.noreply.github.com>
2025-12-05 20:54:43 +0300

    feat: add class Lexem and classes of heirs: ConstantLexem VariableLexem BracketLexem FunctionLexem OperatorLexem

---
+++ lib_lexem/lexem.h
@@ -0,0 +1,79 @@
+// Copyright 2025 Smirnov Nikita
+
+#ifndef LIB_LEXEM_LEXEM_H_
+#define LIB_LEXEM_LEXEM_H_
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <sstream>
+
+enum TypeLexem { Constant, Variable, OpenBracket, ClosedBracket,
+  Function, Operator };
+
+class Lexem {
+  std::string _name;
+  TypeLexem _type;
+
+ public:
+  Lexem() : _name(""), _type(Constant) {}
+  Lexem(const std::string& name, TypeLexem type) : _name(name), _type(type) {}
+
+  std::string getName() const noexcept { return _name; }
+  TypeLexem getType() const noexcept { return _type; }
+};
+
+class ConstantLexem : public Lexem {
+  double _value;
+
+ public:
+  ConstantLexem(double value) : Lexem(doubleToString(value), Constant),
+    _value(value) {};
+
+  double getValue() const noexcept { return _value; }
+
+private:
+  static std::string doubleToString(double value) {
+    std::ostringstream oss;
+    oss << std::fixed << std::setprecision(6) << value;
+    return oss.str();
+  }
+};
+
+class VariableLexem : public Lexem {
+ public:
+  VariableLexem(const std::string& name) : Lexem(name, Variable) {}
+};
+
+class BracketLexem : public Lexem {
+  bool _isOpen;
+
+ public:
+   BracketLexem(const std::string& name, bool isOpen)
+     : Lexem(name, isOpen ? OpenBracket : ClosedBracket), _isOpen(isOpen) {}
+
+   bool isOpen() const noexcept { return _isOpen; }
+};
+
+using MathFunctionPtr = double(*)(double);
+
+class FunctionLexem : public Lexem {
+  MathFunctionPtr _function;
+
+ public:
+   FunctionLexem(const std::string& name, MathFunctionPtr function)
+     : Lexem(name, Function), _function(function) {}
+
+   MathFunctionPtr getFunction() const noexcept { return _function; }
+};
+
+class OperatorLexem : public Lexem {
+  int _priority;
+ public:
+   OperatorLexem(const std::string& name, int priority = -1)
+     : Lexem(name, Operator), _priority(priority) {}
+
+   int getPriority() const noexcept { return _priority; }
+};
+
+#endif  // LIB_LEXEM_LEXEM_H_
